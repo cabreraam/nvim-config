880cfcdbef5d8e3228bf6cf3d0038320069a3605
Anthony Cabrera <cabreraam@ieee.org>
2024-12-02 19:15:37 -0600

    add none-ls configs for cpp and latex formatting

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -13,10 +13,12 @@ return {
 				null_ls.builtins.formatting.stylua,
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort,
+				null_ls.builtins.formatting.clang_format,
 				null_ls.builtins.completion.spell,
 				require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
 				require("none-ls.diagnostics.cpplint"), -- requires none-ls-extras.nvim
 				require("none-ls.formatting.mdslw"), -- requires none-ls-extras.nvim
+				require("none-ls.formatting.latexindent"), -- requires none-ls-extras.nvim
 			},
 			on_attach = function(client, bufnr)
 				if client.supports_method("textDocument/formatting") then
