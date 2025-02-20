4410810a7b3d130fc8625a8591110a046afdcfd6
Anthony Cabrera <cabreraam33@gmail.com>
2025-02-20 16:12:05 -0600

    remove redundant keybinding and add dependency on nvim-dap-virtual-text

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -4,29 +4,23 @@ return {
 		"mfussenegger/nvim-dap-python",
 		"rcarriga/nvim-dap-ui",
 		"nvim-neotest/nvim-nio",
-		--"theHamsta/nvim-dap-virtual-text",
+		"theHamsta/nvim-dap-virtual-text",
 	},
 	config = function()
 		local dap = require("dap")
 		local dapui = require("dapui")
 		local dap_python = require("dap-python")
-		--local dap_virtual_text = require("nvim-dap-virtual-text")
+		local dap_virtual_text = require("nvim-dap-virtual-text")
 
 		dapui.setup({})
-		--dap_virtual_text.setup({
-		--	commented = true,
-		--})
+		dap_virtual_text.setup({
+			commented = true,
+		})
 
 		dap_python.setup("python")
 
 		dap.listeners.after.event_initialized["dapui_config"] = function()
 			dapui.open()
 		end
-
-		local opts = { noremap = true, silent = true }
-		-- Toggle DAP UI
-		vim.keymap.set("n", "<leader>du", function()
-			dapui.toggle()
-		end, opts)
 	end,
 }
