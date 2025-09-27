f625cd0cd26540a75327e6cc7c9963b70dd88e94
Anthony Cabrera <cabreraam@ieee.org>
2025-09-27 13:05:47 -0500

    refactor verible lsp
    Signed-off-by: Anthony Cabrera <cabreraam@ieee.org>

---
+++ lsp/verible.lua
@@ -0,0 +1,8 @@
+vim.lsp.config("verible", {
+	filetypes = { "systemverilog", "verilog" },
+	cmd = { "verible-verilog-ls", "--rules_config_search" }, --, "--file_list_path", "./verible.filelist" },
+	root_dir = function(fname)
+		return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
+	end,
+	autostart = true,
+})
--- lua/plugins/verible.lua
+++ lua/plugins/verible.lua
@@ -1,15 +1,5 @@
 local M = {
 	"chipsalliance/verible",
 }
-M.config = function()
-	vim.lsp.config("verible", {
-		filetypes = { "systemverilog", "verilog" },
-		cmd = { "verible-verilog-ls", "--rules_config_search" }, --, "--file_list_path", "./verible.filelist" },
-		root_dir = function(fname)
-			return vim.fs.dirname(vim.fs.find(".git", { path = fname, upward = true })[1])
-		end,
-		autostart = true,
-	})
-end
 
 return M
