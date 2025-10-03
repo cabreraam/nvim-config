073e25a86482022c302241a20134426a387fb30b
Anthony Cabrera <cabreraam@ieee.org>
2025-10-03 16:05:10 -0500

    updates to dap-python.lua
    Signed-off-by: Anthony Cabrera <cabreraam@ieee.org>

--- lua/plugins/dap-python.lua
+++ lua/plugins/dap-python.lua
@@ -1,26 +1,28 @@
 return {
-	"mfussenegger/nvim-dap-python",
-	config = function()
-		--require("dap-python").setup("python")
-		vim.fn.sign_define("DapBreakpoint", {
-			text = "",
-			texthl = "DiagnosticSignError",
-			linehl = "",
-			numhl = "",
-		})
+  "mfussenegger/nvim-dap-python",
+  dependencies = {
+    "mfussenegger/nvim-dap",
+  },
+  config = function()
+    vim.fn.sign_define("DapBreakpoint", {
+      text = "",
+      texthl = "DiagnosticSignError",
+      linehl = "",
+      numhl = "",
+    })
 
-		vim.fn.sign_define("DapBreakpointRejected", {
-			text = "", -- or "❌"
-			texthl = "DiagnosticSignError",
-			linehl = "",
-			numhl = "",
-		})
+    vim.fn.sign_define("DapBreakpointRejected", {
+      text = "", -- or "❌"
+      texthl = "DiagnosticSignError",
+      linehl = "",
+      numhl = "",
+    })
 
-		vim.fn.sign_define("DapStopped", {
-			text = "", -- or "→"
-			texthl = "DiagnosticSignWarn",
-			linehl = "Visual",
-			numhl = "DiagnosticSignWarn",
-		})
-	end,
+    vim.fn.sign_define("DapStopped", {
+      text = "", -- or "→"
+      texthl = "DiagnosticSignWarn",
+      linehl = "Visual",
+      numhl = "DiagnosticSignWarn",
+    })
+  end,
 }
