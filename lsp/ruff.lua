7f7ddfc369246a42739d82b6552eb4e5dd11264a
Anthony Cabrera <cabreraam@ieee.org>
2025-10-25 12:39:17 -0500

    update ruff config to make sure only one plugin is formatting python
    Signed-off-by: Anthony Cabrera <cabreraam@ieee.org>

--- lsp/ruff.lua
+++ lsp/ruff.lua
@@ -1,2 +1,18 @@
-vim.lsp.config("ruff", {})
+local on_attach = function(client, bufnr)
+	-- Auto formatting when saving file
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		buffer = bufnr,
+		callback = function()
+			vim.lsp.buf.format({
+				async = false,
+				filter = function(client)
+					return client.name == "ruff"
+				end,
+			})
+		end,
+	})
+end
+vim.lsp.config("ruff", {
+	on_attach = on_attach,
+})
 vim.lsp.enable("ruff")
