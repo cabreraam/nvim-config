71684582d72a2c20955a83da074a1f84da828350
Anthony Cabrera <cabreraam33@gmail.com>
2026-02-15 15:45:47 -0600

    remove black
    Signed-off-by: Anthony Cabrera <cabreraam33@gmail.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,50 +1,49 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		opts = {},
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"texlab",
-				"bashls",
-				"ruff",
-				"vhdl_ls",
-				"cmake",
-				"pyright",
-				"neocmake",
-				"black",
-			},
-		},
-	},
-	{
-		"jay-babu/mason-nvim-dap.nvim",
-		opts = {
-			ensure_installed = { "cppdbg" }, -- or "codelldb"
-			automatic_installation = true,
-			handlers = {
-				function(config)
-					require("mason-nvim-dap").default_setup(config)
-				end,
-			},
-		},
-	},
-	{
-		"mfussenegger/nvim-lint",
-		config = function()
-			local lint = require("lint")
-			lint.linters_by_ft = {
-				latex = { "vale" },
-			}
-		end,
-	},
-	{
-		"rshkarin/mason-nvim-lint",
-		opts = {
-			automatic_installation = true,
-			ensure_installed = { "cpplint" },
-		},
-	},
+  {
+    "williamboman/mason.nvim",
+    opts = {},
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls",
+        "texlab",
+        "bashls",
+        "ruff",
+        "vhdl_ls",
+        "cmake",
+        "pyright",
+        "neocmake",
+      },
+    },
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    opts = {
+      ensure_installed = { "cppdbg" }, -- or "codelldb"
+      automatic_installation = true,
+      handlers = {
+        function(config)
+          require("mason-nvim-dap").default_setup(config)
+        end,
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    config = function()
+      local lint = require("lint")
+      lint.linters_by_ft = {
+        latex = { "vale" },
+      }
+    end,
+  },
+  {
+    "rshkarin/mason-nvim-lint",
+    opts = {
+      automatic_installation = true,
+      ensure_installed = { "cpplint" },
+    },
+  },
 }
