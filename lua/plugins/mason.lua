e1c61c2ebe030dbf58253bfb4ad868116014749f
Anthony Cabrera <cabreraam33@gmail.com>
2026-03-05 11:38:42 -0600

    remove redundant specification of installing prettierd
    already happens with none-ls.lua
    
    Signed-off-by: Anthony Cabrera <cabreraam33@gmail.com>

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -57,7 +57,7 @@ return {
       "nvimtools/none-ls.nvim",
     },
     opts = {
-      ensure_installed = { "prettierd" },
+      ensure_installed = {},
       automatic_installation = true,
     },
   },
