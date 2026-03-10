7134755561f987bca5c70df04391474efc998b52
Anthony Cabrera <cabreraam@ieee.org>
2026-03-10 17:37:59 -0500

    give rootmarkers and filetypes to basedpyright
    Signed-off-by: Anthony Cabrera <cabreraam@ieee.org>

--- lsp/basedpyright.lua
+++ lsp/basedpyright.lua
@@ -1,4 +1,7 @@
 return {
+	cmd = { "basedpyright-langserver", "--stdio" },
+	filetypes = { "python" },
+	root_markers = { "pyproject.toml", "setup.py", "setup.cfg", "requirements.txt", "Pipfile", ".git" },
 	settings = {
 		basedpyright = {
 			analysis = {
